2c4494bf927a12062da7a5a5772a2d1e740b4993
Greg <putz.gregory@gmail.com>
2012-06-14 15:30:04 +0200

    corr bug

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		EbookController.viewType type;
 		EbookController controller;
 		EbookModel model;
-		String in = "/media/READER", out = "/tmp/";
+		String in = "/media/READER/", out = "/tmp/";
 
 		type = EbookController.viewType.SWT;
 		for (int i = 0; i < args.length; i++) 
